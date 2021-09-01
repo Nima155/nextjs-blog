6e8cd22ce8d689f98560eca753d18c6fe0c8201f
Nima155 <nimashadab@hotmail.co.uk>
2021-09-01 12:01:24 +0430

    next.js tutorial project

--- pages/index.js
+++ pages/index.js
@@ -1,209 +1,102 @@
 import Head from 'next/head'
-
-export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className="logo" />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
-  )
+import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
+import { getSortedPostsData } from '../lib/posts'
+export default function Home({ allPostsData }) {
+	// A page is a react component exported from a file in the "pages" directory, "Home" in this case
+	return (
+		<Layout home>
+			<Head>
+				<title>{siteTitle}</title>
+			</Head>
+			<section className={utilStyles.headingMd}>
+				<p>Just a programming enthusiast from Iran</p>
+				<p>
+					(This is a sample website - you’ll be building a site like this on{' '}
+					<a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+				</p>
+			</section>
+			<section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+				<h2 className={utilStyles.headingLg}>Blog</h2>
+				<ul className={utilStyles.list}>
+					{allPostsData.map(({ id, date, title }) => (
+						<li className={utilStyles.listItem} key={id}>
+							{title}
+							<br />
+							{id}
+							<br />
+							{date}
+						</li>
+					))}
+				</ul>
+			</section>
+		</Layout>
+	)
 }
+// pre-rendering with data
+export async function getStaticProps() {
+	const allPostsData = getSortedPostsData()
+	return {
+		props: {
+			allPostsData, // Passed to Home as a prop
+		},
+	}
+}
+// By default next.js pre-renders every page. This means that Next.js generates
+// HTML for each page in advance
+
+// Each HTML is associated with minimal JS code necessary for that page.
+// When a page is laoded by the browser, its js code runs and makes the page
+// fully interactive (this is called hydration)
+
+// we can test prerendering by disabling JS in the browser and then refreshing..
+// with normal react apps, nothing would be rendered but with next... our website would still render
+
+/* 
+There are 2 forms of pre-rendering:
+	static generation: 
+		- HTML is generated at build time. The pre-rendered HTML is then reused
+		on each request
+
+	Server side rendering:
+		- Generates HTML on each request
+
+Per-page basis:
+	- We can choose which pre-rendering form to use for each page
+
+When to use SG vs SSR:
+	- If a page could be pre-rendered ahead of a user's request then choose SG. Prefer the use of SG wherever possible
+
+	- If a page shows frequently updated data, and the page content changes on every request use SSR
+	It will be slower, but that's the price you pay! 
+*/
+
+/*
+Static generation with and without data:
+	- Pages that don't require external data will automatically be statically generated when 
+	the app is built for production
+
+	- However, for some pages, we might need to first fetch some external data
+	in order to be able to render the HTML. 
+
+	SG with Data using "getStaticProps":
+		- When exporting a page component we can also export an async function called
+		*getStaticProps*. This function runs at build time in production and inside it we can
+		fetch any external data and send it as props to the page
+
+		- This essentially tells next.js that this page has some data dependencies - so when 
+		you pre-render this page at build time, make sure to resolve them first
+
+		- getStaticProps only runs on the server-side. It will never run on the 
+		client side. It won't even be included in the JS bundler for the browser.
+		This means that we can write code such as direct database queries without the 
+		being sent to browsers.
+
+		- Productions vs development:
+			- In production *getStaticProps* runs at build time, but this behavior can be
+			enhanced by using the `fallback` key returned by *getStaticPaths*
+
+			- In development, *getStaticProps* runs on every request.
+	
+	 	- *getStaticProps* can only be exported from a page
+*/
